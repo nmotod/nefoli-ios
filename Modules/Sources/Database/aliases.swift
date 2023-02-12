b7a58b6b0efe41548c09e54f504db8c1522b9d95
morn5 <toobu9b+code@gmail.com>
2023-02-12 15:52:18 +0900

    feat(db): add aliases for Realm types

---
+++ Modules/Sources/Database/aliases.swift
@@ -0,0 +1,8 @@
+import Foundation
+import RealmSwift
+
+public typealias List = RealmSwift.List
+public typealias NotificationToken = RealmSwift.NotificationToken
+public typealias ObjectChange = RealmSwift.ObjectChange
+public typealias RealmCollectionChange = RealmSwift.RealmCollectionChange
+public typealias ObservedRealmObject = RealmSwift.ObservedRealmObject
