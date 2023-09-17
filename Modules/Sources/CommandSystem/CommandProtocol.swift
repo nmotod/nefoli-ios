f4251d26c35d829da0f66bd51c30e99c646ed782
nmotod <toobu9b+code@gmail.com>
2023-09-17 18:36:14 +0900

    refactor(command): move common props to protocol

--- Modules/Sources/CommandSystem/CommandProtocol.swift
+++ Modules/Sources/CommandSystem/CommandProtocol.swift
@@ -2,18 +2,22 @@ import Foundation
 import UIKit
 
 public protocol CommandProtocol: Identifiable where ID == String {
+    static var idPrefix: String { get }
+
     var definition: CommandDefinition { get }
 
-    static var idPrefix: String { get }
+    var title: String { get }
+
+    var subtitle: String? { get }
+
+    var image: UIImage? { get }
+
+    var discoverabilityTitle: String? { get }
 
     func makeUIAction(handler: @escaping UIActionHandler) -> UIAction
 }
 
-extension CommandProtocol where Self: RawRepresentable, RawValue == String {
-    public var id: String {
-        return "\(type(of: self).idPrefix).\(rawValue)"
-    }
-
+extension CommandProtocol {
     public var title: String {
         return definition.title
     }
@@ -38,5 +42,11 @@ extension CommandProtocol where Self: RawRepresentable, RawValue == String {
             discoverabilityTitle: discoverabilityTitle,
             handler: handler
         )
+    }
+}
+
+extension CommandProtocol where Self: RawRepresentable, RawValue == String {
+    public var id: String {
+        return "\(type(of: self).idPrefix).\(rawValue)"
     }
 }
