d0d4897315900ff8e5c77698d28a0ff40b0e770e
nmotod <toobu9b+code@gmail.com>
2023-09-19 13:58:07 +0900

    feat(search): guess URL or query

--- Modules/Sources/TabBrowser/search.swift
+++ Modules/Sources/TabBrowser/search.swift
@@ -1,12 +1,33 @@
 import Foundation
 
-func buildSearchURL(text: String) -> URL? {
-    if let url = URL(string: text) {
-        return url
+enum InputType {
+    case url(URL)
+    case query(String)
+}
+
+func guessInputType(text: String) -> InputType {
+    let spaceRegex = try! Regex(#"\s"#)
+    if text.contains(spaceRegex) {
+        return .query(text)
     }
 
-    var urlComponents = URLComponents(string: "https://www.google.com/search")!
-    urlComponents.queryItems = [URLQueryItem(name: "q", value: text)]
+    let httpRegex = try! Regex("^https?://")
+    if text.contains(httpRegex), let url = URL(string: text) {
+        return .url(url)
+    }
 
-    return urlComponents.url
+    return .query(text)
+}
+
+func buildSearchURL(text: String) -> URL? {
+    switch guessInputType(text: text) {
+    case let .url(url):
+        return url
+
+    case let .query(text):
+        var urlComponents = URLComponents(string: "https://www.google.com/search")!
+        urlComponents.queryItems = [URLQueryItem(name: "q", value: text)]
+
+        return urlComponents.url
+    }
 }
