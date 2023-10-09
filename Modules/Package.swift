27784ddb5fc0467117208ae19ff1ca34ad685868
nmotod <toobu9b+code@gmail.com>
2023-10-09 15:55:25 +0900

    refactor: restructure of module directory structures

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -2,11 +2,7 @@
 
 import PackageDescription
 
-let commonDependenciesForTest: [Target.Dependency] = [
-    .product(name: "Difference", package: "Difference"),
-]
-
-let package = Package(
+var package = Package(
     name: "Modules",
     defaultLocalization: "en",
     platforms: [
@@ -17,163 +13,138 @@ let package = Package(
             name: "Root",
             targets: ["Root"]
         ),
-        .library(
-            name: "SettingsUI.Previews",
-            targets: ["SettingsUI"]
-        ),
-        .library(
-            name: "Bookmarks.Previews",
-            targets: ["Bookmarks"]
-        ),
-        .library(
-            name: "TabManageUI.Previews",
-            targets: ["TabManageUI"]
-        ),
-        .library(
-            name: "Utils.Previews",
-            targets: ["Utils"]
-        ),
-        .library(
-            name: "Theme.Previews",
-            targets: ["Theme"]
-        ),
-        .library(
-            name: "TabBrowser.Previews",
-            targets: ["TabBrowser"]
-        ),
+        // `products` will be added via defineModule()
     ],
     dependencies: [
-        // .package(url: "https://github.com/mischa-hildebrand/AlignedCollectionViewFlowLayout", revision: "49330ef67177dba5c9e1a3efdd0df93d83f12ee7"),
-        // .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", revision: "0.1.4"),
-        //        .package(url: "https://github.com/realm/SwiftLint.git", revision: "0.50.3"),
-        //        .package(url: "https://github.com/apple/swift-format", branch: "main"),
-
         .package(url: "https://github.com/SnapKit/SnapKit", revision: "5.6.0"),
         .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.2"),
         .package(url: "https://github.com/realm/realm-swift.git", from: "10.33.0"),
         .package(url: "https://github.com/krzysztofzablocki/Difference.git", from: "1.0.2"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.16.0"),
-    ],
-    targets: [
-        .target(
-            name: "Root",
-            dependencies: [
-                // .product(name: "AlignedCollectionViewFlowLayout", package: "AlignedCollectionViewFlowLayout"),
-                // .product(name: "SnapKit", package: "SnapKit"),
-                // .product(name: "Introspect", package: "SwiftUI-Introspect"),
-                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
-                .target(name: "Database"),
-                .target(name: "TabBrowser"),
-            ]
-        ),
-        .target(
-            name: "Database",
-            dependencies: [
-                .product(name: "Realm", package: "realm-swift"),
-                .product(name: "RealmSwift", package: "realm-swift"),
-            ]
-        ),
-        .testTarget(
-            name: "DatabaseTests",
-            dependencies: [
-                .target(name: "Database"),
-            ] + commonDependenciesForTest
-        ),
-        .target(
-            name: "TabBrowser",
-            dependencies: [
-                .product(name: "SnapKit", package: "SnapKit"),
-                .target(name: "Database"),
-                .target(name: "Theme"),
-                .target(name: "Utils"),
-                .target(name: "Bookmarks"),
-                .target(name: "SettingsUI"),
-                .target(name: "TabManageUI"),
-                .target(name: "CommandSystem"),
-            ]
-        ),
-        .testTarget(
-            name: "TabBrowserTests",
-            dependencies: [
-                .target(name: "TabBrowser"),
-            ] + commonDependenciesForTest
-        ),
-        .target(
-            name: "Theme",
-            exclude: [
-                "swiftgen.yml",
-            ],
-            resources: [
-                .process("Resources"),
-            ],
-            plugins: [
-                .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin"),
-            ]
-        ),
-        .target(
-            name: "Utils",
-            dependencies: [
-                .product(name: "SnapKit", package: "SnapKit"),
-                .target(name: "Theme"),
-            ]
-        ),
-        .testTarget(
-            name: "UtilsTests",
-            dependencies: [
-                .target(name: "Utils"),
-            ]
-        ),
-        .target(
-            name: "Bookmarks",
-            dependencies: [
-                .target(name: "Database"),
-                .target(name: "Theme"),
-                .target(name: "Utils"),
-            ]
-        ),
-        .testTarget(
-            name: "BookmarksTests",
-            dependencies: [
-                .target(name: "Bookmarks"),
-            ]
-        ),
-        .target(
-            name: "SettingsUI",
-            dependencies: [
-                .target(name: "Database"),
-                .target(name: "Theme"),
-                .target(name: "Utils"),
-                .target(name: "ContentBlocker"),
-            ]
-        ),
-        .testTarget(
-            name: "SettingsUITests",
-            dependencies: [
-                .target(name: "SettingsUI"),
-            ]
-        ),
-        .target(
-            name: "ContentBlocker",
-            dependencies: [
-                .target(name: "Database"),
-            ]
-        ),
-        .testTarget(
-            name: "ContentBlockerTests",
-            dependencies: [
-                .target(name: "ContentBlocker"),
-            ]
-        ),
-        .target(
-            name: "TabManageUI",
-            dependencies: [
-                .target(name: "Database"),
-                .target(name: "Theme"),
-            ]
-        ),
-        .target(
-            name: "CommandSystem",
-            dependencies: []
-        ),
     ]
+    // `targets` will be added via defineModule()
 )
+
+enum ModuleCategory: String {
+    case Components
+    case Features
+    case Utils
+}
+
+defineModule(.Components, .target(
+    name: "Root",
+    dependencies: [
+        // .product(name: "AlignedCollectionViewFlowLayout", package: "AlignedCollectionViewFlowLayout"),
+        // .product(name: "SnapKit", package: "SnapKit"),
+        // .product(name: "Introspect", package: "SwiftUI-Introspect"),
+        .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+        .target(name: "Database"),
+        .target(name: "TabBrowser"),
+    ]
+))
+
+defineModule(.Components, .target(
+    name: "Database",
+    dependencies: [
+        .product(name: "Realm", package: "realm-swift"),
+        .product(name: "RealmSwift", package: "realm-swift"),
+    ]
+))
+
+defineModule(.Components, .target(
+    name: "Theme",
+    exclude: [
+        "swiftgen.yml",
+    ],
+    resources: [
+        .process("Resources"),
+    ],
+    plugins: [
+        .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin"),
+    ]
+), previews: true)
+
+defineModule(.Features, .target(
+    name: "TabBrowser",
+    dependencies: [
+        .product(name: "SnapKit", package: "SnapKit"),
+        .target(name: "Database"),
+        .target(name: "Theme"),
+        .target(name: "Utils"),
+        .target(name: "Bookmarks"),
+        .target(name: "SettingsUI"),
+        .target(name: "TabManageUI"),
+        .target(name: "CommandSystem"),
+    ]
+), previews: true)
+
+defineModule(.Features, .target(
+    name: "Bookmarks",
+    dependencies: [
+        .target(name: "Database"),
+        .target(name: "Theme"),
+        .target(name: "Utils"),
+    ]
+), previews: true)
+
+defineModule(.Features, .target(
+    name: "SettingsUI",
+    dependencies: [
+        .target(name: "Database"),
+        .target(name: "Theme"),
+        .target(name: "Utils"),
+        .target(name: "ContentBlocker"),
+    ]
+), previews: true)
+
+defineModule(.Features, .target(
+    name: "ContentBlocker",
+    dependencies: [
+        .target(name: "Database"),
+    ]
+))
+
+defineModule(.Features, .target(
+    name: "TabManageUI",
+    dependencies: [
+        .target(name: "Database"),
+        .target(name: "Theme"),
+    ]
+), previews: true)
+
+defineModule(.Utils, .target(
+    name: "Utils",
+    dependencies: [
+        .product(name: "SnapKit", package: "SnapKit"),
+        .target(name: "Theme"),
+    ]
+), previews: true)
+
+defineModule(.Utils, .target(
+    name: "CommandSystem",
+    dependencies: []
+), tests: false)
+
+func defineModule(_ category: ModuleCategory, _ target: Target, tests: Bool = true, previews: Bool = false) {
+    target.path = "\(category)/\(target.name)"
+
+    package.targets.append(target)
+
+    if tests {
+        package.targets.append(.testTarget(
+            name: "\(target.name)Tests",
+            dependencies: [
+                .target(name: target.name),
+                .product(name: "Difference", package: "Difference"),
+            ],
+            path: "\(category)/\(target.name)Tests"
+        ))
+    }
+
+    if previews {
+        package.products.append(.library(
+            name: target.name + ".Previews",
+            targets: [target.name]
+        ))
+    }
+}
