a5b793bd35c6d58e92a0333849829675ef66c71c
nmotod <toobu9b+code@gmail.com>
2024-04-29 11:17:14 +0900

    refactor: Target.with()

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -2,7 +2,162 @@
 
 import PackageDescription
 
-var package = Package(
+extension Target.Dependency {
+    static let SnapKit: Target.Dependency = .product(name: "SnapKit", package: "SnapKit")
+}
+
+var generatedProducts: [Product] = []
+var generatedTargets: [Target] = []
+
+let baseTargets: [Target] = [
+    .target(
+        name: "AppEntryPoint",
+        dependencies: [
+            .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+            "Database",
+            "TabBrowser",
+        ]
+    ).with(),
+
+    .target(
+        name: "Database",
+        dependencies: [
+            .product(name: "Realm", package: "realm-swift"),
+            .product(name: "RealmSwift", package: "realm-swift"),
+        ]
+    ).with(
+        testCustom: { t in
+            t.resources = [.process("Resources")]
+        }
+    ),
+
+    .executableTarget(
+        name: "Database_v17",
+        dependencies: [
+            .product(name: "Realm", package: "realm-swift"),
+            .product(name: "RealmSwift", package: "realm-swift"),
+        ]
+    ).with(),
+
+    .target(
+        name: "ThemeSystem",
+        exclude: [
+            "swiftgen.yml",
+        ],
+        resources: [
+            .process("Resources"),
+        ],
+        plugins: [
+            .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin"),
+        ]
+    ).with(
+        preview: true
+    ),
+
+    .target(
+        name: "Bookmark",
+        dependencies: [
+            "Database",
+            "ThemeSystem",
+            "Utils",
+        ]
+    ).with(
+        test: true,
+        preview: true
+    ),
+
+    .target(
+        name: "Settings",
+        dependencies: [
+            "Database",
+            "ThemeSystem",
+            "Utils",
+            "ContentBlocker",
+        ]
+    ).with(
+        test: true,
+        preview: true
+    ),
+
+    .target(
+        name: "ContentBlocker",
+        dependencies: [
+            "Database",
+        ]
+    ).with(
+        test: true
+    ),
+
+    .target(
+        name: "MenuSheet",
+        dependencies: [
+            "ThemeSystem",
+            "Utils",
+        ]
+    ).with(
+        preview: true
+    ),
+
+    .target(
+        name: "TabBrowserCore",
+        dependencies: [
+            .SnapKit,
+            "Database",
+            "ThemeSystem",
+            "Utils",
+            "Bookmark",
+            "MenuSheet",
+            "ActionSystem",
+            "WebViewStickyInteraction",
+        ]
+    ).with(
+        test: true,
+        preview: true
+    ),
+
+    .target(
+        name: "TabManager",
+        dependencies: [
+            "TabBrowserCore",
+        ]
+    ).with(
+        test: true,
+        preview: true
+    ),
+
+    .target(
+        name: "TabBrowser",
+        dependencies: [
+            "TabBrowserCore",
+            "TabManager",
+            "Settings",
+        ]
+    ).with(),
+
+    .target(
+        name: "Utils",
+        dependencies: [
+            .SnapKit,
+        ]
+    ).with(
+        test: true,
+        preview: true
+    ),
+
+    .target(
+        name: "ActionSystem",
+        dependencies: []
+    ).with(),
+
+    .target(
+        name: "WebViewStickyInteraction",
+        dependencies: [
+            .SnapKit,
+        ]
+    ).with(),
+]
+
+let package = Package(
     name: "Modules",
     defaultLocalization: "en",
     platforms: [
@@ -13,201 +168,50 @@ var package = Package(
             name: "AppEntryPoint",
             targets: ["AppEntryPoint"]
         ),
-        // `products` will be added via defineModule()
-    ],
+        .executable(
+            name: "db-fixture-gen-v17",
+            targets: ["Database_v17"]
+        ),
+    ] + generatedProducts,
     dependencies: [
         .package(url: "https://github.com/SnapKit/SnapKit", revision: "5.6.0"),
         .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.2"),
         .package(url: "https://github.com/realm/realm-swift.git", from: "10.33.0"),
         .package(url: "https://github.com/krzysztofzablocki/Difference.git", from: "1.0.2"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.16.0"),
-    ]
-    // `targets` will be added via defineModule()
+    ],
+    targets: baseTargets + generatedTargets
 )
 
-enum ModuleCategory: String {
-    case EntryPoints
-    case Components
-    case Features
-    case Utils
-}
+extension Target {
+    func with(
+        test: Bool = false,
+        testCustom: ((inout Target) -> Void)? = nil,
+        preview: Bool = false
+    ) -> Self {
+        if test || testCustom != nil {
+            var testTarget = Target.testTarget(
+                name: "\(name)Tests",
+                dependencies: [
+                    .target(name: name),
+                    .product(name: "Difference", package: "Difference"),
+                ]
+            )
 
-let noTests: (Target) -> Target? = { _ in nil }
+            if let testCustom {
+                testCustom(&testTarget)
+            }
 
-defineModule(.EntryPoints, .target(
-    name: "AppEntryPoint",
-    dependencies: [
-        // .product(name: "AlignedCollectionViewFlowLayout", package: "AlignedCollectionViewFlowLayout"),
-        // .product(name: "SnapKit", package: "SnapKit"),
-        // .product(name: "Introspect", package: "SwiftUI-Introspect"),
-        .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
-        .target(name: "Database"),
-        .target(name: "TabBrowser"),
-    ]
-), test: noTests)
+            generatedTargets.append(testTarget)
+        }
 
-defineModule(.Components, .target(
-    name: "Database",
-    dependencies: [
-        .product(name: "Realm", package: "realm-swift"),
-        .product(name: "RealmSwift", package: "realm-swift"),
-    ]
-), test: { (target: Target) in
-    target.resources = [.process("Resources")]
-    return target
-})
+        if preview {
+            generatedProducts.append(.library(
+                name: name + ".Previews",
+                targets: [name]
+            ))
+        }
 
-defineModule(.Components, .executableTarget(
-    name: "Database_v17",
-    dependencies: [
-        .product(name: "Realm", package: "realm-swift"),
-        .product(name: "RealmSwift", package: "realm-swift"),
-    ]
-), test: noTests)
-
-package.products.append(.executable(
-    name: "db-fixture-gen-v17",
-    targets: ["Database_v17"]
-))
-
-defineModule(.Components, .target(
-    name: "ThemeSystem",
-    exclude: [
-        "swiftgen.yml",
-    ],
-    resources: [
-        .process("Resources"),
-    ],
-    plugins: [
-        .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin"),
-    ]
-), test: noTests, previews: true)
-
-defineModule(.Components, .target(
-    name: "Bookmark",
-    dependencies: [
-        .target(name: "Database"),
-        .target(name: "ThemeSystem"),
-        .target(name: "Utils"),
-    ]
-), previews: true)
-
-defineModule(.Components, .target(
-    name: "Settings",
-    dependencies: [
-        .target(name: "Database"),
-        .target(name: "ThemeSystem"),
-        .target(name: "Utils"),
-        .target(name: "ContentBlocker"),
-    ]
-), previews: true)
-
-defineModule(.Components, .target(
-    name: "ContentBlocker",
-    dependencies: [
-        .target(name: "Database"),
-    ]
-))
-
-defineModule(.Components, .target(
-    name: "MenuSheet",
-    dependencies: [
-        .target(name: "ThemeSystem"),
-        .target(name: "Utils"),
-    ]
-), test: noTests, previews: true)
-
-defineModule(.Components, .target(
-    name: "TabBrowserCore",
-    dependencies: [
-        .product(name: "SnapKit", package: "SnapKit"),
-        .target(name: "Database"),
-        .target(name: "ThemeSystem"),
-        .target(name: "Utils"),
-        .target(name: "Bookmark"),
-        .target(name: "MenuSheet"),
-        .target(name: "ActionSystem"),
-        .target(name: "WebViewStickyInteraction"),
-    ]
-), previews: true)
-
-defineModule(.Components, .target(
-    name: "TabManager",
-    dependencies: [
-        .target(name: "TabBrowserCore"),
-    ]
-), previews: true)
-
-defineModule(.Features, .target(
-    name: "TabBrowser",
-    dependencies: [
-        .target(name: "TabBrowserCore"),
-        .target(name: "TabManager"),
-        .target(name: "Settings"),
-    ]
-), test: noTests, previews: false)
-
-defineModule(.Utils, .target(
-    name: "Utils",
-    dependencies: [
-        .product(name: "SnapKit", package: "SnapKit"),
-    ]
-), previews: true)
-
-defineModule(.Utils, .target(
-    name: "ActionSystem",
-    dependencies: []
-), test: noTests)
-
-defineModule(.Utils, .target(
-    name: "WebViewStickyInteraction",
-    dependencies: [
-        .product(name: "SnapKit", package: "SnapKit"),
-    ]
-), test: noTests)
-
-func defineModule(
-    _ category: ModuleCategory,
-    _ target: Target,
-    test: ((Target) -> Target?)? = nil,
-    previews: Bool = false
-) {
-//    target.path = "Sources/\(category)/\(target.name)"
-
-    package.targets.append(target)
-
-//    if tests {
-//        package.targets.append(.testTarget(
-//            name: "\(target.name)Tests",
-//            dependencies: [
-//                .target(name: target.name),
-//                .product(name: "Difference", package: "Difference"),
-//            ] + testDependencies,
-//            path: "\(category)Tests/\(target.name)Tests"
-//        ))
-//    }
-
-    var testTarget: Target? = .testTarget(
-        name: "\(target.name)Tests",
-        dependencies: [
-            .target(name: target.name),
-            .product(name: "Difference", package: "Difference"),
-        ]
-//        path: "Tests/\(category)/\(target.name)Tests"
-    )
-
-    if let test {
-        testTarget = test(testTarget!)
-    }
-
-    if let testTarget {
-        package.targets.append(testTarget)
-    }
-
-    if previews {
-        package.products.append(.library(
-            name: target.name + ".Previews",
-            targets: [target.name]
-        ))
+        return self
     }
 }
